a25dd00f830ef489ffd7109595819209bd8b18e0
hitaro2k <vlad.bojenko2005@gmail.com>
2023-05-07 22:56:43 +0300

    add to-response

--- js/modules/views.js
+++ js/modules/views.js
@@ -168,8 +168,6 @@ export function views() {
               countElem.textContent = item.count;
               updateTotalPrice();
             }
-           
-       
           });
         });
        
