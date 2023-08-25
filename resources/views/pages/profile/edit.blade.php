7253eeb5ade187014f942f799b30fb4d8020de52
hitaro2k <vlad.bojenko2005@gmail.com>
2023-08-25 17:29:28 +0300

    add sumb btn

--- resources/views/pages/profile/edit.blade.php
+++ resources/views/pages/profile/edit.blade.php
@@ -17,6 +17,8 @@
           <div class="input-wrapper"><input type="text" placeholder = "По батькові"></div>
           <div class="input-wrapper"><input type="text" placeholder = "Прізвище"></div>
           <div class="input-wrapper"><input type="text" placeholder = "Пошта"></div>
+
+          <button type="submit">Зберегти</button>
         </form>
       </div>
         
@@ -25,13 +27,19 @@
           <p class="title">Зміна пароля</p>
           <div class="input-wrapper"><input type="text" placeholder = "Новий пароль"></div>
           <div class="input-wrapper"><input type="text" placeholder = "Підтвердіть новий пароль"></div>
+
+          <button type="submit">Зберегти</button>
         </form>
       </div>
-      
+     
       <div class="form-wrapper">
-        <form action="" class="form-delete">
-          <p class="title">Видалити аккаунт</p>
-          <div class="input-wrapper"><button type="submit">Видалити</button></div>
+      <form method="post" action="{{ route('profile.destroy') }}" class="p-6"> 
+        <p class="title">Видалити аккаунт</p>
+            @csrf
+            @method('delete')
+                <x-danger-button class="ml-3">
+                    {{ __('Delete Account') }}
+                </x-danger-button>
         </form>
       </div>
       
