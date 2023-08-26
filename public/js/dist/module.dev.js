fbe27d87b74c247d00573d126c7f819f977b42b5
hitaro2k <vlad.bojenko2005@gmail.com>
2023-08-26 16:36:53 +0300

    rw

--- public/js/dist/module.dev.js
+++ public/js/dist/module.dev.js
@@ -4,4 +4,6 @@ var _views = require("../js/modules/logic/views.js");
 
 var _animation = _interopRequireDefault(require("../js/modules/animation/animation.js"));
 
+var _search = require("../js/modules/logic/search.js");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
--- public/js/module.js
+++ public/js/module.js
@@ -1,2 +1,3 @@
 import { views } from "../js/modules/logic/views.js";
 import defAnimation from "../js/modules/animation/animation.js";
+import {search} from "../js/modules/logic/search.js"
